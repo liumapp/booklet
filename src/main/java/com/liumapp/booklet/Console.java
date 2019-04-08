3a5dec3d781f3c3a9f55f05e044410b6bc14b9ef
liumapp <liumapp.com@gmail.com>
2019-04-08 17:15:51 +0800

    add src folder

---
+++ src/main/java/com/liumapp/booklet/Console.java
@@ -0,0 +1,12 @@
+package com.liumapp.booklet;
+
+/**
+ * file Console.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/4/8
+ */
+public class Console {
+}
