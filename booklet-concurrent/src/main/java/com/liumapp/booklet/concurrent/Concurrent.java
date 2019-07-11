0d29df70aa7a6986f47e590d86bab675f37be533
liumapp <liumapp.com@gmail.com>
2019-07-11 16:57:28 +0800

    add resources folder

---
+++ booklet-concurrent/src/main/java/com/liumapp/booklet/concurrent/Concurrent.java
@@ -0,0 +1,21 @@
+package com.liumapp.booklet.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file Concurrent.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/11
+ */
+@SpringBootApplication
+public class Concurrent
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Concurrent.class, args);
+    }
+}
