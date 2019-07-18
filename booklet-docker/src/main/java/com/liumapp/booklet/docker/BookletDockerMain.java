3a592315c7c400d401167439899904271e529f78
liumapp <liumapp.com@gmail.com>
2019-07-18 10:55:42 +0800

    add booklet docker main class

---
+++ booklet-docker/src/main/java/com/liumapp/booklet/docker/BookletDockerMain.java
@@ -0,0 +1,21 @@
+package com.liumapp.booklet.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file BookletDockerMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/18
+ */
+@SpringBootApplication
+public class BookletDockerMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(BookletDockerMain.class, args);
+    }
+
+}
