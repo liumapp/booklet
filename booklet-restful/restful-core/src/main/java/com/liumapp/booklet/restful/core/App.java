e5301c27aa9d047fefee827b757b722503bfb7b9
liumapp <liumapp.com@gmail.com>
2019-07-23 10:31:07 +0800

    add three modules

---
+++ booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.booklet.restful.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
