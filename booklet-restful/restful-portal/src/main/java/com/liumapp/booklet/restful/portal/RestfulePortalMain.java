d4dedb539ead621c3dda71ec330b0d2ab46c1231
liumapp <liumapp.com@gmail.com>
2019-07-23 10:42:48 +0800

    update restfule portal modules : main class info

---
+++ booklet-restful/restful-portal/src/main/java/com/liumapp/booklet/restful/portal/RestfulePortalMain.java
@@ -0,0 +1,19 @@
+package com.liumapp.booklet.restful.portal;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * file RestfulePortalMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/23
+ */
+public class RestfulePortalMain
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(RestfulePortalMain.class, args);
+    }
+}
