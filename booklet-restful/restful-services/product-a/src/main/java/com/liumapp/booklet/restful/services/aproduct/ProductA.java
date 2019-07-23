9b3475d43b1403bb7ec7c4543abe3e9d7bcf99ce
liumapp <liumapp.com@gmail.com>
2019-07-23 14:45:24 +0800

    update product a and product b module main class

---
+++ booklet-restful/restful-services/product-a/src/main/java/com/liumapp/booklet/restful/services/aproduct/ProductA.java
@@ -0,0 +1,17 @@
+package com.liumapp.booklet.restful.services.aproduct;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file ProductA.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/23
+ */
+@SpringBootApplication
+public class ProductA
+{
+
+}
---
+++ booklet-restful/restful-services/product-b/src/main/java/com/liumapp/booklet/restful/services/bproduct/ProductB.java
@@ -0,0 +1,17 @@
+package com.liumapp.booklet.restful.services.bproduct;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file ProductB.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/23
+ */
+@SpringBootApplication
+public class ProductB
+{
+
+}
