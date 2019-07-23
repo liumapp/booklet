7498507bbd50d7e570c7fd4dc10a79edc8e706f8
liumapp <liumapp.com@gmail.com>
2019-07-23 16:21:47 +0800

    add prodcut a controller class

---
+++ booklet-restful/restful-services/product-a/src/main/java/com/liumapp/booklet/restful/services/aproduct/controller/ProductAController.java
@@ -0,0 +1,20 @@
+package com.liumapp.booklet.restful.services.aproduct.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * file ProductAController.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/23
+ */
+@RestController
+@RequestMapping("/proa")
+public class ProductAController {
+
+
+
+}
