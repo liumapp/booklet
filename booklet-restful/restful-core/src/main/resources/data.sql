de437af563daff5a121bf90fede5c0174c3a77fa
liumapp <liumapp.com@gmail.com>
2019-07-23 15:56:23 +0800

    update h2 db info

---
+++ booklet-restful/restful-core/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO producta(name, price) VALUES('Audi', 52642);
+INSERT INTO producta(name, price) VALUES('Mercedes', 57127);
+INSERT INTO producta(name, price) VALUES('Skoda', 9000);
+INSERT INTO producta(name, price) VALUES('Volvo', 29000);
+INSERT INTO productb(name, price) VALUES('Iphone', 350000);
+INSERT INTO productb(name, price) VALUES('MacbookPro', 21000);
+INSERT INTO productb(name, price) VALUES('Ipad', 41400);
+INSERT INTO productb(name, price) VALUES('MacbookAir', 21600);
