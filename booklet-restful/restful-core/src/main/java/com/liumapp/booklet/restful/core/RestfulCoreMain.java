af712421740ebe22fdcf8c463b6ea21045ca8795
liumapp <liumapp.com@gmail.com>
2019-07-23 14:15:46 +0800

    update restful core main class

---
+++ booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/RestfulCoreMain.java
@@ -0,0 +1,17 @@
+package com.liumapp.booklet.restful.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file RestfulCoreMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/23
+ */
+@SpringBootApplication
+public class RestfulCoreMain
+{
+
+}
