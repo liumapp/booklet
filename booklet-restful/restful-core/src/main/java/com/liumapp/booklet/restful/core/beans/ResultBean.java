2ae5a68e3804d8a247c26d41474740d0cfaa6555
liumapp <liumapp.com@gmail.com>
2019-07-23 16:46:54 +0800

    add userinfo db

--- booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/beans/ResultBean.java
+++ booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/beans/ResultBean.java
@@ -17,15 +17,22 @@ public class ResultBean<T> implements Serializable {
 
 	private static final long serialVersionUID = -8145738728023739799L;
 
-	public static final int NO_LOGIN = -1;
+	public static final int SUCCESS = 10000;
 
-	public static final int SUCCESS = 0;
+	/**
+	 * 检查产品id异常
+	 */
+	public static final int CHECK_ID_FAIL = 10001;
 
-	public static final int CHECK_FAIL = 1;
+	/**
+	 * 检查产品名称异常
+	 */
+	public static final int CHECK_NAME_FAIL = 10002;
 
-	public static final int NO_PERMISSION = 2;
-
-	public static final int UNKNOWN_EXCEPTION = -99;
+	/**
+	 * 未知异常
+	 */
+	public static final int UNKNOWN_EXCEPTION = 99;
 
 	/**
 	 * 返回的信息(主要出错的时候使用)
@@ -34,10 +41,9 @@ public class ResultBean<T> implements Serializable {
 
 	/**
 	 * 接口返回码, 0表示成功, 其他看对应的定义
-	 * 晓风轻推荐的做法是: 
-	 * 0   : 成功
-	 * >0 : 表示已知的异常(例如提示错误等, 需要调用地方单独处理) 
-	 * <0 : 表示未知的异常(不需要单独处理, 调用方统一处理)
+	 * 10000: 成功
+	 * >10000 : 表示已知的异常(例如提示错误等, 需要调用地方单独处理)
+	 * <10000 : 表示未知的异常(不需要单独处理, 调用方统一处理)
 	 */
 	private int code = SUCCESS;
 
