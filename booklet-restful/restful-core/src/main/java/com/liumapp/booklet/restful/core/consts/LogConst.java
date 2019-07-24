76b537d53aa45efb045ffeeb9514e6c68a7cc216
liumapp <liumapp.com@gmail.com>
2019-07-24 14:30:52 +0800

    add log const class

---
+++ booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/consts/LogConst.java
@@ -0,0 +1,30 @@
+package com.liumapp.booklet.restful.core.consts;
+
+/**
+ * 日志注解里面的常量，自己增加即可
+ *
+ * file LogConst.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/24
+ */
+public class LogConst {
+
+    String ACTION_ADD = "添加操作";
+
+    String ACTION_DELETE = "删除操作";
+
+    String ACTION_UPDATE = "更新操作";
+
+    String ACTION_QUERY= "查询操作";
+
+    /**
+     *  对象类型
+     */
+    String ITEM_TYPE_PRODUCT_A = "产品a";
+
+    String ITEM_TYPE_PRODUCT_B = "产品b";
+
+}
--- booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/util/UserUtil.java
+++ booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/util/UserUtil.java
@@ -32,7 +32,6 @@ public class UserUtil {
 
     public static void setUser(String userid) {
         tlUser.set(userid);
-
         // 把用户信息放到log4j
         MDC.put(KEY_USER, userid);
     }
