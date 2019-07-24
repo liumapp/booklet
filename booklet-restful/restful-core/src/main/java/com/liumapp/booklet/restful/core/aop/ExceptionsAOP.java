634921fa9f76edffb0b4b2707c786ad6dd6e6db2
liumapp <liumapp.com@gmail.com>
2019-07-24 17:19:28 +0800

    add exceptions aop class

---
+++ booklet-restful/restful-core/src/main/java/com/liumapp/booklet/restful/core/aop/ExceptionsAOP.java
@@ -0,0 +1,20 @@
+package com.liumapp.booklet.restful.core.aop;
+
+/**
+ *
+ * 处理和包装异常
+ *
+ * 注意：所有异常一律抛出处理，不允许catch
+ *
+ * file ExceptionsAOP.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/7/24
+ */
+public class ExceptionsAOP {
+
+
+
+}
