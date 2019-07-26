def44e936fabee04f92ca8d636c907f510f6c282
liumapp <liumapp.com@gmail.com>
2019-07-26 16:46:57 +0800

    rm unnecessary code

--- booklet-restful/restful-core/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO product_a(id, name, price) VALUES(1, 'Audi', 52642);
-INSERT INTO product_a(id, name, price) VALUES(2, 'Mercedes', 57127);
-INSERT INTO product_a(id, name, price) VALUES(3, 'Skoda', 9000);
-INSERT INTO product_a(id, name, price) VALUES(4, 'Volvo', 29000);
-INSERT INTO product_b(id, name, price) VALUES(1, 'Iphone', 350000);
-INSERT INTO product_b(id, name, price) VALUES(2, 'MacbookPro', 21000);
-INSERT INTO product_b(id, name, price) VALUES(3, 'Ipad', 41400);
-INSERT INTO product_b(id, name, price) VALUES(4, 'MacbookAir', 21600);
-INSERT INTO users(id, name, phone, appId, appSecret) VALUES(1, 'liumapp', '15757125631', 'yourAppId', 'yourAppSecret');
