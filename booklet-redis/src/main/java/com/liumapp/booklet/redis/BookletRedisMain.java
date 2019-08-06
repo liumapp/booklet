406b03cbfb2bd1db3f37f8d808c3ec4d05cb039b
liumapp <liumapp.com@gmail.com>
2019-08-06 14:19:51 +0800

    update booklet redis dependencies

---
+++ booklet-redis/src/main/java/com/liumapp/booklet/redis/BookletRedisMain.java
@@ -0,0 +1,19 @@
+package com.liumapp.booklet.redis;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * file BookletRedisMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/8/6
+ */
+public class BookletRedisMain
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BookletRedisMain.class, args);
+    }
+}
---
+++ booklet-redis/src/test/java/com/liumapp/booklet/redis/BookletRedisMainTest.java
@@ -0,0 +1,11 @@
+package com.liumapp.booklet.redis;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class BookletRedisMainTest {
+
+}
