c58465922d141d0dd987867913ad00514ba319ba
liumapp <liumapp.com@gmail.com>
2019-11-05 11:24:35 +0800

    update booklet design pattern main class

---
+++ booklet-design-pattern/src/main/java/com/liumapp/design/pattern/BookletDesignPattern.java
@@ -0,0 +1,20 @@
+package com.liumapp.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file BookletDesignPattern.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/11/5
+ */
+@SpringBootApplication
+public class BookletDesignPattern
+{
+    public static void main(String[] args) {
+        SpringApplication.run(BookletDesignPattern.class);
+    }
+}
