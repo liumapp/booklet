baaeabbe18cb1c369469c411944b6785f1068523
liumapp <liumapp.com@gmail.com>
2019-11-05 11:23:30 +0800

    add booklet design pattern module

---
+++ booklet-design-pattern/src/main/java/com/liumapp/design/pattern/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.design.pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
