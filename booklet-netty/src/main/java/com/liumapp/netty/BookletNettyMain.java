3595ae68b9a84bf3ebaff5c275304eb1e512dd6f
liumapp <liumapp.com@gmail.com>
2019-11-06 14:48:10 +0800

    add booklet netty module

---
+++ booklet-netty/src/main/java/com/liumapp/netty/BookletNettyMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file BookletNettyMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/11/6
+ */
+@SpringBootApplication
+public class BookletNettyMain
+{
+    public static void main(String[] args) {
+        SpringApplication.run(BookletNettyMain.class);
+    }
+}
