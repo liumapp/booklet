2b78e6ed3377934b07e4d64e24ce3e6e050173c2
liumapp <liumapp.com@gmail.com>
2019-11-18 17:03:23 +0800

    add consumer

---
+++ booklet-kafka/src/main/java/com/liumapp/kafka/consumer/ConsumerTest.java
@@ -0,0 +1,45 @@
+package com.liumapp.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * file ConsumerTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/11/18
+ */
+public class ConsumerTest {
+
+    public static void main(String[] args) {
+        String topicName = "my-topic";
+        String groupId = "my-group";
+
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topicName));//订阅kafka
+        try {
+            while (true) {
+                
+            }
+        } finally {
+            consumer.close();
+        }
+
+    }
+
+}
