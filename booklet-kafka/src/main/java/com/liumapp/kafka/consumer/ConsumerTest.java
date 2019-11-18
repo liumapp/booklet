643b519a784ec277f6445776a658a06e9cd23edd
liumapp <liumapp.com@gmail.com>
2019-11-18 20:22:26 +0800

    add kafka helper tool

--- booklet-kafka/src/main/java/com/liumapp/kafka/consumer/ConsumerTest.java
+++ booklet-kafka/src/main/java/com/liumapp/kafka/consumer/ConsumerTest.java
@@ -1,9 +1,13 @@
 package com.liumapp.kafka.consumer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -15,14 +19,15 @@ import java.util.Properties;
  * homepage http://www.liumapp.com
  * date 2019/11/18
  */
+@Slf4j
 public class ConsumerTest {
 
     public static void main(String[] args) {
-        String topicName = "my-topic";
-        String groupId = "my-group";
+        String topicName = "my_topic";
+        String groupId = "my_group";
 
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.189.132:9092");
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
@@ -34,12 +39,15 @@ public class ConsumerTest {
         consumer.subscribe(Arrays.asList(topicName));//订阅kafka
         try {
             while (true) {
-                
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record: records) {
+                    log.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
+                }
             }
         } finally {
             consumer.close();
         }
-
+        
     }
 
 }
