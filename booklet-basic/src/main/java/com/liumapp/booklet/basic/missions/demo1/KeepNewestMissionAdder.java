57695dc9950c0a39377e58c8a70f08b9f679c78b
liumapp <liumapp.com@gmail.com>
2019-11-21 20:43:13 +0800

    add keep newest mission adder

---
+++ booklet-basic/src/main/java/com/liumapp/booklet/basic/missions/demo1/KeepNewestMissionAdder.java
@@ -0,0 +1,22 @@
+package com.liumapp.booklet.basic.missions.demo1;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+/**
+ *
+ * 只保留最新任务的添加者
+ *
+ * file KeepNewestMissionAdder.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/11/21
+ */
+public class KeepNewestMissionAdder <T> implements MissionAdder {
+
+    @Override
+    public void add(PriorityBlockingQueue queue, Object o) {
+        
+    }
+}
