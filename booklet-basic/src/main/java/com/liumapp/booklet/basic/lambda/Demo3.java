70c490e840af9c6e7ec6580e6d4e4d5b59ff5768
liumapp <liumapp.com@gmail.com>
2019-12-16 11:05:16 +0800

    add demo3

---
+++ booklet-basic/src/main/java/com/liumapp/booklet/basic/lambda/Demo3.java
@@ -0,0 +1,19 @@
+package com.liumapp.booklet.basic.lambda;
+
+/**
+ * 将给定的一组用户中，名称为lisi的用户打包
+ *
+ * file Demo3.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/12/16
+ */
+public class Demo3 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
