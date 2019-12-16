2c782b2b14b2988e46e6abea5167cc0abf46be49
liumapp <liumapp.com@gmail.com>
2019-12-16 14:11:25 +0800

    lambda demo3 done

--- booklet-basic/src/main/java/com/liumapp/booklet/basic/lambda/Demo3.java
+++ booklet-basic/src/main/java/com/liumapp/booklet/basic/lambda/Demo3.java
@@ -1,5 +1,14 @@
 package com.liumapp.booklet.basic.lambda;
 
+import com.alibaba.fastjson.JSON;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 /**
  * 将给定的一组用户中，名称为lisi的用户打包
  *
@@ -13,7 +22,28 @@ package com.liumapp.booklet.basic.lambda;
 public class Demo3 {
 
     public static void main(String[] args) {
-        
+        List<Person> personList = new ArrayList<>();
+        Consumer<Person> personConsumer = x -> {
+            if (x.name.equals("lisi")) {
+                personList.add(x);
+            }
+        };
+        Stream.of(new Person(21, "zhangsan"),
+                new Person(22, "lisi"),
+                new Person(23, "lisi"),
+                new Person(24, "wangwu")).forEach(personConsumer);
+
+        System.out.println(JSON.toJSON(personList));
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Person {
+
+        Integer id;
+
+        String name;
+
     }
 
 }
